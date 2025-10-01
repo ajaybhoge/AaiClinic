b1825dc45af91ee27c8c2e33f618d68c887c66e4
ajaybhoge <ajaybhoge43@gmail.com>
2025-10-01 10:26:33 +0530

    Updated a minor changes

---
+++ .next/static/chunks/app/about/page-160a32814df52231.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,462,718,822],{1159:(e,s,n)=>{Promise.resolve().then(n.bind(n,658)),Promise.resolve().then(n.bind(n,8856)),Promise.resolve().then(n.bind(n,5182)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[166,566,441,684,358],()=>s(1159)),_N_E=e.O()}]);
